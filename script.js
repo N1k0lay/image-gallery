829bc841130b97e6e501c7f53d16dd05710d1772
n1k0lay <kolia.d97@gmail.com>
2023-09-29 17:46:45 +0500

    feat: add search function

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ fetch(`https://api.unsplash.com/photos?per_page=12`, {
     .catch(error => console.error(error));
 
 const gridImages = document.querySelector('.grid_images');
-function viewImages(response) {
-    console.log(response)
-    const images = response;
-
+function viewImages(images) {
+    gridImages.innerHTML = '';
     images.forEach(item => {
         const imgWrapper = document.createElement('div');
         imgWrapper.classList.add('img_Wrapper')
@@ -19,3 +17,18 @@ function viewImages(response) {
         gridImages.append(imgWrapper);
     })
 }
+
+document.querySelector('input').focus();
+
+document.addEventListener('keyup', (e) => {
+    const input = document.querySelector('input');
+
+    if(e.key === 'Enter' && input.value) {
+        fetch(`https://api.unsplash.com/search/photos?query=${input.value}&per_page=12`, {
+            headers: {'Authorization': 'Client-ID 2vqpX83uFEKv0dDseumwYo4cF9dm5atv6bdQUj0fgYU'}
+        })
+            .then(res => res.json())
+            .then(data => viewImages(data.results))
+            .catch(error => console.error(error));
+    }
+})
